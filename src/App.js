e7e86074897d10e84ae209c932f2915b7aad51d5
Ed <lookatemail@gmail.com>
2021-09-15 10:47:36 +0100

    Refactor SerachBox component & geocoding API request

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import logo from '../src/assets/logo.svg';
 import '../src/css/App.css';
+
+import AirlineSeatReclineExtraSharpIcon from '@material-ui/icons/AirlineSeatReclineExtraSharp';
+import SearchBar from './components/SearchBar';
+import Data from '../src/components/Data.json';
 
 export default function App({ props }) {
   return (
     <div style={styles.container}>
       <div style={styles.header}>
-        <img src={logo} className="App-logo" alt="logo" />
-        <div style={{ fontSize: 36, fontWeight: 'bold' }}>
-          React Search Box Component
-        </div>
+        <AirlineSeatReclineExtraSharpIcon
+          className="App-logo"
+          alt="logo"
+          style={{ fontSize: 60, color: 'red' }}
+        />
+        <div style={styles.title}>React Search Box Component</div>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <SearchBar placeholder="Search Bar" data={Data} />
+      </div>
+
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: 20 }}>
+        hello content
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: 20 }}>
+        hello content
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: 20 }}>
+        hello content
       </div>
     </div>
   );
@@ -28,5 +46,11 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: 50,
+  },
+  title: {
+    fontSize: 36,
+    padding: 20,
+    fontWeight: 'bold',
   },
 };
---
+++ src/components/SearchBar.js
@@ -0,0 +1,177 @@
+import React from 'react';
+
+import SearchIcon from '@material-ui/icons/Search';
+import CloseIcon from '@material-ui/icons/Close';
+
+export default function SearchBar({ placeholder, data }) {
+  const [filteredData, setFilteredData] = React.useState([]);
+  const [searchPhrase, setSearchPhrase] = React.useState('');
+
+  const SEARCH_BOX_WIDTH = 330;
+  const BORDER_RADIUS = 24;
+  const INPUT_CONTAINER_HEIGHT = 40;
+  const INPUT_PADDING = '0px 24px';
+  const bgPrimary = '#fff';
+  const bgSecondary = '#f2f2f2';
+
+  // HANDLERS ---------------------------------------------------------
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    setSearchPhrase(searchWord);
+  };
+
+  const handleClearInput = () => {
+    setFilteredData([]);
+    setSearchPhrase('');
+  };
+
+  const fetchData = async () => {
+    const location = searchPhrase;
+    const URL = 'https://maps.googleapis.com/maps/api/geocode/json';
+    const API_KEY = 'AIzaSyBqK6x1Nj8GpbVCHEy1aicJAYN8Hf61iOE';
+    const REQUEST_URL =
+      URL + '?address=' + location.replace(/ /g, '+') + 'CA&key=' + API_KEY;
+
+    try {
+      // const response = await fetch(REQUEST_URL);
+      // const data = await response.json();
+      // const address = data.results[0].address_components;
+      // const address = data.results[0].formatted_address;
+      // console.log('address: ', address); //debug
+      // setFilteredData(data);
+
+      const newFilter = data.filter((value) => {
+        return value.title.toLowerCase().includes(searchPhrase.toLowerCase());
+      });
+      if (searchPhrase === '') {
+        setFilteredData([]);
+      } else {
+        setFilteredData(newFilter);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      fetchData();
+      console.log(filteredData);
+    }, 1500);
+  }, [searchPhrase]);
+
+  // SERVERS ---------------------------------------------------------
+  const FeedElement = ({ post }) => {
+    const { title } = post;
+
+    let titlePreview = `${title.substring(0, 35)}...`;
+    if (title.length < 35) titlePreview = title;
+
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          padding: '10px 0',
+        }}
+      >
+        <div>{titlePreview}</div>
+      </div>
+    );
+  };
+
+  const ServeFilterSelectionContainer = () => {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          position: 'absolute',
+          zIndex: 99,
+          width: SEARCH_BOX_WIDTH,
+          borderBottomLeftRadius: BORDER_RADIUS,
+          borderBottomRightRadius: BORDER_RADIUS,
+          // marginTop: INPUT_CONTAINER_HEIGHT / 4,
+          backgroundColor: bgPrimary,
+          overflow: 'hidden',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            flex: 1,
+            maxHeight: '20vh',
+            borderTop: `1px solid ${bgSecondary}`,
+            padding: INPUT_PADDING,
+          }}
+        >
+          <div style={{ overflowY: 'auto' }}>
+            {filteredData.map((post, index) => (
+              <FeedElement post={post} key={index} />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  // RETURN ---------------------------------------------------------
+  return (
+    <div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'end',
+          borderRadius: BORDER_RADIUS,
+          borderBottomLeftRadius: !!filteredData.length ? '' : BORDER_RADIUS,
+          borderBottomRightRadius: !!filteredData.length ? '' : BORDER_RADIUS,
+          height: 50,
+          width: SEARCH_BOX_WIDTH,
+          backgroundColor: 'white',
+          overflow: 'hidden',
+        }}
+      >
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={searchPhrase}
+          onChange={handleFilter}
+          style={styles.input}
+        />
+        <div style={styles.iconContainer}>
+          <div
+            style={{
+              display: 'flex',
+              padding: 5,
+              borderRadius: '50%',
+              backgroundColor: bgSecondary,
+            }}
+          >
+            {!searchPhrase.length && <SearchIcon />}
+            {!!searchPhrase.length && <CloseIcon onClick={handleClearInput} />}
+          </div>
+        </div>
+      </div>
+      {!!filteredData.length && <ServeFilterSelectionContainer />}
+    </div>
+  );
+}
+
+const styles = {
+  input: {
+    display: 'flex',
+    flex: 1,
+    fontSize: 18,
+    padding: '0 20px',
+    border: 'none',
+    outline: 'none',
+    backgroundColor: 'transparent',
+  },
+  iconContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    cursor: 'pointer',
+    padding: '0 10px',
+  },
+};
